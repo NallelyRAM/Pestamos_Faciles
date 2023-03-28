4059c976f3444cf88029e61dcac49324d695d561
NallelyRAM <nallely.ramirez216724@potros.itson.edu.mx>
2023-03-27 18:19:50 -0700

    des

---
+++ app/src/main/java/mx/itson/edu/prestamosfaciles/SeleccionProducto.kt
@@ -0,0 +1,11 @@
+package mx.itson.edu.prestamosfaciles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeleccionProducto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccion_producto)
+    }
+}
