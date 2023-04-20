c67823b5f4233787eedfbef0f57fc2d8c217b538
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-04-20 15:50:31 -0700

    Carcasa de la pantalla agregar producto

---
+++ app/src/main/java/mx/itson/edu/prestamosfaciles/AgregarProductoActivity.kt
@@ -0,0 +1,16 @@
+package mx.itson.edu.prestamosfaciles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class AgregarProductoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_producto)
+    }
+
+
+
+}
