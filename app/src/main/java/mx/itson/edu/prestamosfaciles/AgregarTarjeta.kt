8102592624c8abaaba6bf753b8521199aa1f7f8f
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-05-06 00:35:03 -0700

    Diseño layout agregar tarjeta

---
+++ app/src/main/java/mx/itson/edu/prestamosfaciles/AgregarTarjeta.kt
@@ -0,0 +1,11 @@
+package mx.itson.edu.prestamosfaciles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarTarjeta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_tarjeta)
+    }
+}
