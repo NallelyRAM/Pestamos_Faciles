47ede78f67a551d44745ded47ca2237e1042d7a1
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-05-13 20:52:11 -0700

    Diseño pantallas rentadores, detalle rentador y plantilla rentada

---
+++ app/src/main/java/mx/itson/edu/prestamosfaciles/Actividades/detalle_rentador.kt
@@ -0,0 +1,12 @@
+package mx.itson.edu.prestamosfaciles.Actividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.itson.edu.prestamosfaciles.R
+
+class detalle_rentador : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_rentador)
+    }
+}
